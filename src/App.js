f3a5f5b063957dbddab878d4608bf52a4645262b
amh092 <ahmedalhazza@Ahmeds-MacBook-Pro.local>
2022-11-07 10:26:48 +0300

    useeffect and mobile dice shadow box fix

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import './App.css';
 import Advice from './components/Advice/Advice';
-import normalize from 'normalize.css'
-import {useState} from 'react'
+
+import {useState,useEffect} from 'react'
 function App() {
 
- function fetchMoviesList(){
-   fetch('https://api.adviceslip.com/advice')
-   .then((response) => {
-     return response.json();
-   })
-   .then((data) => {
-    setAdvice(data.slip.advice)
-    setAdviceID(data.slip.id)
-       });
-     
-   
-      }
+  const [advice, setAdvice] = useState('')
+  const [adviceID, setAdviceID] = useState('')
+  function fetchMoviesList(){
+    
+    fetch('https://api.adviceslip.com/advice')
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+     setAdvice(data.slip.advice)
+     setAdviceID(data.slip.id)
+        });
       
+    
+       }
+useEffect(() => {
 fetchMoviesList()
+}, [advice])
 
   return (
     <div className="App">
-      <Advice advice={advice} id={id} action={fetchMoviesList}/>
+      <Advice advice={advice} id={adviceID} action={fetchMoviesList}/>
     </div>
   );
 }
